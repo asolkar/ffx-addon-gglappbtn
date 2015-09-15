b6db53a6919ecaa37921ebefc43e1e3f2e683d5b
Mahesh Asolkar <mahesh@mahesha.com>
2015-09-14 22:03:01 -0700

    New icons, apps
    Added 3 new apps - Wallet, Hangouts, Contacts. Updated app icons with
    new Google icons.

--- data/gapps-info.js
+++ data/gapps-info.js
@@ -9,56 +9,71 @@ var gapps_info = {
   "gplus": {
     "url" : "https://plus.google.com/",
     "desc" : "Google+",
-    "iconpos" : "-21px -1489px"
+    "iconpos" : "0px -1863px"
   },
   "gmail": {
     "url" : "https://gmail.com/",
     "desc" : "Gmail",
-    "iconpos" : "0px -418px"
+    "iconpos" : "0px -966px"
   },
   "gcal": {
     "url" : "https://www.google.com/calendar/",
     "desc" : "Calendar",
-    "iconpos" : "0px -104px"
+    "iconpos" : "0px -758px"
   },
   "gdoc": {
     "url" : "https://docs.google.com/",
     "desc" : "Docs",
-    "iconpos" : "0px -1731px"
+    "iconpos" : "0px -1794px"
   },
   "gsheets": {
     "url" : "https://sheets.google.com/",
     "desc" : "Sheets",
-    "iconpos" : "0px -2150px"
+    "iconpos" : "0px -828px"
   },
   "gdrv": {
     "url" : "https://drive.google.com/",
     "desc" : "Drive",
-    "iconpos" : "0px -1420px"
+    "iconpos" : "0px -1518px"
   },
   "gphotos": {
     "url" : "https://photos.google.com/",
     "desc" : "Photos",
-    "iconpos" : "0px -2219px"
+    "iconpos" : "0px -690px"
   },
   "gmaps": {
     "url" : "https://maps.google.com/",
     "desc" : "Maps",
-    "iconpos" : "0px -1977px"
+    "iconpos" : "0px -68px"
   },
   "gplay": {
     "url" : "https://play.google.com/",
     "desc" : "Play",
-    "iconpos" : "0px -1143px"
+    "iconpos" : "0px -1242px"
   },
   "gytube": {
     "url" : "https://youtube.com/",
     "desc" : "YouTube",
-    "iconpos" : "0px -1835px"
+    "iconpos" : "0px -1932px"
   },
   "gnews": {
     "url" : "https://news.google.com/",
     "desc" : "News",
-    "iconpos" : "0px -1350px"
+    "iconpos" : "0px -484px"
+  },
+  "gwall": {
+    "url" : "https://wallet.google.com/manage/?tab=wa",
+    "desc" : "Wallet",
+    "iconpos" : "0px -414px"
+  },
+  "ghang": {
+      "url" : "https://hangouts.google.com/",
+      "desc" : "Hangouts",
+      "iconpos" : "0px 0px"
+  },
+  "gcont": {
+      "url" : "https://contacts.google.com/",
+      "desc" : "Contacts",
+      "iconpos" : "0px -1726px"
   }
 };
